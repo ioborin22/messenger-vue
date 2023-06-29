a139d3acbff3efae61c9338b63b0815842c96a19
ioborin22 <ioborin22@gmail.com>
2023-06-29 19:45:08 -0400

    20230629

--- src/components/UserMessages.vue
+++ src/components/UserMessages.vue
@@ -1,10 +1,10 @@
+
 <template>
   <div>
-    <ul>
-      <li v-for="message in messages" :key="message.id">
-        {{ message.text_message }}
-      </li>
-    </ul>
+    <div v-for="message in messages" :key="message.id" class="message-bubble" :class="{ 'sender': message.sender_id === 1, 'receiver': message.sender_id !== 1 }">
+      <div class="message-text">{{ message.text_message }}<span class="date" v-if="message.flag === 0"> (unread)</span></div>
+      <div class="message-time">{{ message.created_at }}</div>
+    </div>
   </div>
 </template>
 
@@ -53,4 +53,49 @@ export default {
 </script>
 
 <style scoped>
+.message-container {
+  display: flex;
+  flex-direction: column;
+  padding-top: 60px;
+  padding-bottom: 65px;
+
+  position: absolute;
+  top: 80px;
+  /* replace 70px with the height of your chat-header section */
+  width: 100%;
+}
+
+.message-bubble {
+  max-width: 80%;
+  padding: 10px;
+  margin: 10px;
+  border-radius: 10px;
+  clear: both;
+  word-wrap: break-word;
+}
+
+.message-bubble img {
+  max-width: 100%;
+  height: auto;
+}
+
+.sender {
+  align-self: flex-end;
+  background-color: #e3e3e3e3;
+}
+
+.receiver {
+  align-self: flex-start;
+  background-color: #333333;
+}
+
+.message-text {
+  font-size: 16px;
+}
+
+.message-time {
+  font-size: 12px;
+  margin-top: 5px;
+  color: #999;
+}
 </style>
